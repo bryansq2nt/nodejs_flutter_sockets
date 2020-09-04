1517054235b1cce8ef9ca8c94ef2465f2df8d6b5
Bryan Murgas <bryan_murgasvasquez@hotmail.com>
2020-09-03 20:57:28 -0600

    V1.0.0

--- sockets/sockets.js
+++ sockets/sockets.js
@@ -1,16 +1,38 @@
 const { io } = require('../server');
+const Bands = require('../models/bands');
+const Band = require('../models/band');
+
+const bands = new Bands();
+
+bands.addBand( new Band('Queen'));
+bands.addBand( new Band('Bon Jovi'));
+bands.addBand( new Band('Metallica'));
+bands.addBand( new Band('Heroes del silencio'));
+
 
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands',bands.getBands());
 
     client.on('disconnect',() => {
         console.log('Cliente desconectado');
     });
 
-    client.on('mensaje', ( payload ) => {
-        console.log('Mensaje: ',payload);
-
-        io.emit('mensaje', { admin : 'Nuevo mensaje'});
-        
+  
+    client.on('vote-band',(payload) => {
+        bands.voteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
     });
+
+    client.on('new-band',(payload) => {
+        bands.addBand( new Band(payload.name));
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('delete-band',(payload) => {
+        bands.deleteBand( payload.id );
+        io.emit('active-bands', bands.getBands() );
+    });
+    
 });
